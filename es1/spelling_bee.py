bffdab327ec61836176fde62750e2935e6de0b41
Francesco03W <francescoveronese16@gmail.com>
2025-05-09 10:42:12 +0200

    rifatti

--- es1/spelling_bee.py
+++ es1/spelling_bee.py
@@ -1,50 +1,25 @@
-#differenza tra uses_all e uses_only:
-#uses_all: controllo se una parola usa TUTTE le lettere disponibili.
-#uses_only: controllo se una parola usa SOLO le lettere disponibili (e nessune altre)
-
-def uses_all(word,required):
+def score_word(word, available):
     """
-    Check whether a word uses all the required letters
+    Compute the score for an acceptable word
 
-	word : word to be checked
-	required: string of the required letter 
-    
-    >>> uses_all('banana', 'ban')
-    True
-    >>> uses_all('apple', 'api')
-    False
-    """
-    
-    for required_letter in required.lower():
-        used_letter=False
-        for letter in word.lower():
-            if letter == required_letter:
-                used_letter=True
-        if used_letter == False:
-            return False
-    return True
-
-def uses_only(word,available):
-    """
-    Checks whether a word uses only the available letters
-
-	word : word to be checked
+	word : word to be scored
 	available : string of the available letters
     
-    >>> uses_only('banana', 'ban')
-    True
-    >>> uses_only('apple', 'apl')
-    False
+    >>> score_word('card', 'ACDLORT')
+    1
+    >>> score_word('color', 'ACDLORT')
+    5
+    >>> score_word('cartload', 'ACDLORT')
+    15
     """
-    for letter in word.lower():
-        valid=False
-        for available_letter in available.lower():
-            if letter == available_letter:
-                valid=True
-        if valid == False:
-            return False
-    return True
-
+    length=len(word)
+    if length == 4:
+        return 1
+    if length > 4:
+        if uses_all(word,available.lower()):
+            return length+7
+        else:
+            return length
 
 def check_word(word, available, required):
     """
@@ -65,58 +40,61 @@ def check_word(word, available, required):
     >>> check_word('bee', 'ACDLORT', 'R')
     False
     """
-
-    if len(word)<4:
+    if len(word) < 4:
         return False
-    
-    # si può scrivere usando if .... in ....
-    valid = uses_all(word,required) and uses_only(word,available)
-    if valid == True:
+
+    if (uses_only(word,available.lower()) and uses_all(word,required.lower())):
         return True
-    else:
-        return False
+    return False
 
-def score_word(word, available):
+
+def uses_all(word, required):
     """
-    Compute the score for an acceptable word
+    Check whether a word uses all the required letters
 
-	word : word to be scored
+	word : word to be checked
+	required: string of the required letter
+
+    >>> uses_all('banana', 'ban')
+    True
+    >>> uses_all('apple', 'api')
+    False
+    """
+    
+    for letter in required:
+        if letter not in word:
+            return False
+    return True
+
+def uses_only(word, available):
+    """
+    Checks whether a word uses only the available letters
+
+	word : word to be checked
 	available : string of the available letters
     
-    >>> score_word('card', 'ACDLORT')
-    1
-    >>> score_word('color', 'ACDLORT')
-    5
-    >>> score_word('cartload', 'ACDLORT')
-    15
+    >>> uses_only('banana', 'ban')
+    True
+    >>> uses_only('apple', 'apl')
+    False
     """
-    points=0
-    if len(word) < 4:
-        return 0
-    if len(word) == 4:
-        return 1
-    if len(word) > 4:
-        points+=len(word)
-        pangram=uses_all(word,available)
-        if pangram == True:
-            points+=7
-        return points
-#problema: line include il \n!
-def spelling_bee():
-    total_score=0
-    for line in open("words.txt"):
-        word=line[:len(line)-1]
-        word_score=0
-        valid_word=check_word(word,'ACDLORT','R')
-        if valid_word == True:
-            word_score=score_word(word,'ACDLORT')
-            print(word,word_score)
-            total_score+=word_score
-    print("Total score:",total_score)
-
-if __name__ == "__main__":
-	spelling_bee()
     
+    for letter in word:
+        if letter not in available:
+            return False
+    return True
 
-
+def main():
+    f=open("words.txt","r")
+    total_score=0
+    for word in f:
+        clean_word=word.strip()
+        if check_word(clean_word,'ACDLORT','R'):
+            score=score_word(clean_word,"ACDLORT")
+            total_score+=score
+            print(f'{clean_word} {score}')
+    print(f'Total score {total_score}')
+        
+if __name__ == "__main__":
+    main()
 
--- es2/inspector.py
+++ es2/inspector.py
@@ -8,27 +8,28 @@ def clean_book(source_filename, destination_filename):
     >>> clean_book("stevenson.txt", "stevenson_clean.txt")
     2530
     """
-    f_input=open(source_filename)
+    f_input=open(source_filename,'r')
     f_output=open(destination_filename,'w')
+    cleaning=False
+    lines=0
 
+    header="*** START OF THIS PROJECT GUTENBERG EBOOK THE STRANGE CASE OF DR. ***"
+    footer="*** END OF THIS PROJECT GUTENBERG EBOOK THE STRANGE CASE OF DR. ***"
 
-    header=True # in intestazione
-    # tail=False non è necessario, ritorno appena trovo quella linea.
-    lines_written=0
-    for line in f_input:
-        if header == True: #-1 è l'indice dell'ultimo elemento della stringa, ossia il carattere newline \n
-            if line[:-1] == "*** START OF THIS PROJECT GUTENBERG EBOOK THE STRANGE CASE OF DR. ***":
-                header=False
+    for line in f_input: 
+        clean_line=line.strip()
+        if cleaning == True:
+            if clean_line == footer:
+                f_input.close()
+                f_output.close()
+                return lines
+            f_output.write(line)
+            lines+=1
         else:
-            if line[:-1] == "*** END OF THIS PROJECT GUTENBERG EBOOK THE STRANGE CASE OF DR. ***":
-                return lines_written
-            else:
-                f_output.write(line)
-                lines_written+=1
-    f_input.close()
-    f_output.close()
+            if clean_line == header:
+                cleaning = True
+    return 0 #se non trova mai header
 
-# Devo fare controllo case sensitive?
 def count_unique_words(filename):
     """
     Count the number of unique words in a file
@@ -38,21 +39,16 @@ def count_unique_words(filename):
     >>> count_unique_words("stevenson_clean.txt")
     6039
     """
-    unique_words=[]
-    f_input=open(filename)
-    for line in f_input:
-        words_line=[] #contiene le parole della singola linea    
-        #rimuovo il carattere newline che potrebbe creare nuove parole
-        line_clean=line[:-1]
-        words_line=line_clean.split()
+    f=open(filename,'r')
+    unique_words = []
+    for line in f:
+        clean_line=line.strip()
+        words_line=clean_line.split()
         for word in words_line:
-            #attenzione, words_line è una lista, nel nostro caso ogni elemento è str
             if word not in unique_words:
                 unique_words.append(word)
     return len(unique_words)
 
-
-
 if __name__ == "__main__":
     import doctest
-doctest.testmod()
+    doctest.testmod()
